bf4a88fa666ad68a26b2c1cb21ae4485ecc94846
Marlena Broniewicz <bronka1.dev@gmail.com>
2025-09-24 13:29:57 +0200

    Added dark style to bottom bar in android

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,8 @@ import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { AuthProvider } from "@/hooks/useAuth";
+import * as SystemUI from "expo-system-ui";
+import { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 import AppRouter from "./AppRouter";
@@ -17,6 +19,13 @@ import AppRouter from "./AppRouter";
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+  useEffect(() => {
+    SystemUI.setBackgroundColorAsync(
+      colorScheme === "dark"
+        ? DarkTheme.colors.background
+        : DefaultTheme.colors.background
+    );
+  }, [colorScheme]);
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <PaperProvider>
